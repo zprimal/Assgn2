b7af09a765febfffcdbe2a035747cf09750819ec
zprimal <36151080+zprimal@users.noreply.github.com>
2018-05-10 17:35:00 +0800

    Added getPtIn functionality for Rectangle (complete) Updated error codes for return string functions Added getPtOn functionality for Cross (Completed|Truncated)

--- Cross.cpp
+++ Cross.cpp
@@ -18,6 +18,7 @@ Cross::Cross(string setName, string setST, Vertex newVArr[12]){
    for (int i = 0; i < noOfVertices; i++) {
       vtxArray[i] = new Vertex(newVArr[i]);
    }
+   cout << toString() << endl;
 }
 
 Cross::~Cross(){
@@ -46,11 +47,184 @@ string Cross::getVertices(){
 }
 
 string Cross::getPtIn(){
+   string result = "";
+   int xMin, yMin, xMax, yMax;
+   //DID YOU THINK IT WOULD BE THIS EASY?
+   // gets the 4 X-points which the shape touches on TRUNC
+   
+   // gets the 4 Y-points which the shape touches on TRUNC
 
+
+   //Get min/max range of shape
+   for (int i = 0; i < noOfVertices; i++) {
+      int j = i + 1;
+      if (i == noOfVertices-1) {
+         j = 0;
+      }
+      int aX = vtxArray[i]->getVertexX();
+      int bX = vtxArray[j]->getVertexX();
+      int aY = vtxArray[i]->getVertexY();
+      int bY = vtxArray[j]->getVertexY();
+
+      if (i == 0) {
+         xMin = aX;
+         xMax = aX;
+         yMin = aY;
+         yMax = aY;
+      }
+
+      //Gets the max and min points for the shape
+      if (aX < xMin) {
+         xMin = aX;
+      } else if (bX < xMin) {
+         xMin = bX;
+      }
+
+      if (aY < yMin) {
+         yMin = aY;
+      } else if (bY < yMin) {
+         yMin = bY;
+      }
+
+      if (aX > xMax) {
+         xMax = aX;
+      } else if (bX > xMax) {
+         xMax = bX;
+      }
+
+      if (aY > yMax) {
+         yMax = aY;
+      } else if (bY > yMax) {
+         yMax = bY;
+      }
+   } // END of for loop
+
+   //Get the max and min points for the X and Y ranges TRUNC
+
+
+   //Get the inbetweens for the XY ranges TRUNC
+
+
+   //Debug TRUNC
+
+
+   // Calculate number of points in Shape TRUNC
+
+
+   Vertex inShapeVtxArr[inShapeSize];
+   int inShapeItr = 0;
+   int xRange = xMax-xMin-1;
+   int yRange = yMax-yMin-1;
+
+   //Obtains points in shape from top-down, left-right TRUNC
+
+
+   for (int i = 0; i < inShapeSize; i++) {
+      result = result + "[" + to_string(inShapeVtxArr[i].getVertexX()) + "," + to_string(inShapeVtxArr[i].getVertexY()) + "]\n";
+   }
+   return result;
 }
 
 string Cross::getPtOn(){
+   int edgePts = 0;
 
+   for (int i = 0; i < noOfVertices; i++) {
+      int j = i + 1;
+      if (i == noOfVertices-1) {
+         j = 0;
+      }
+
+      int pointDist = 0;
+      int aX = vtxArray[i]->getVertexX();
+      int bX = vtxArray[j]->getVertexX();
+      int aY = vtxArray[i]->getVertexY();
+      int bY = vtxArray[j]->getVertexY();
+
+      if (aX == bX) {
+         edgePts = edgePts + abs(aY - bY) - 1;
+      } else if (aY == bY) {
+         edgePts = edgePts + abs(aX - bX) - 1;
+      } else {
+         cout << "Error: shape cannot be computed!" << endl;
+         return "1";
+      }
+   }
+
+   if (!edgePts > 0) {
+      cout << "Error: No points on shape!" << endl;
+      return "1";
+   }
+
+   cout << "edgePts: " << edgePts << endl;
+   Vertex totalVTXArr[edgePts];
+   int totalInt = 0;
+   for (int i = 0; i < noOfVertices; i++) {
+      int j = i + 1;
+      if (i == noOfVertices-1) {
+         j = 0;
+      }
+
+      int pointDist = 0;
+      int aX = vtxArray[i]->getVertexX();
+      int bX = vtxArray[j]->getVertexX();
+      int aY = vtxArray[i]->getVertexY();
+      int bY = vtxArray[j]->getVertexY();
+      string XorY;//Checks which axis is the line parallel to
+
+      //Checks for number of coodinates which describe the edges of the Rectangle
+      if (aX == bX) {
+         pointDist = abs(aY - bY);
+         XorY = "X";
+      } else if (aY == bY) {
+         pointDist = abs(aX - bX);
+         XorY = "Y";
+      } else {
+         cout << "Error: Side is slanted!" << endl;
+         return "1";
+      }
+
+      if (pointDist == 0) {
+         cout << "Error: Side has no points!" << endl;
+         return "1";
+      }
+      // cout << "pointDist: " << pointDist << endl;
+      if (XorY == "X") { //Parallel to the X-axis
+         for (int p = 1; p < pointDist; p++) {
+            if (aY < bY) {
+               // cout << "aX: " << aX << ", aY+p: " << aY+p << "\n";
+               totalVTXArr[totalInt] = Vertex(aX, aY+p);
+               totalInt++;
+            } else if (aY > bY) {
+               // cout << "aX: " << aX << ", bY+p: " << bY+p << "\n";
+               totalVTXArr[totalInt] = Vertex(aX, bY+p);
+               totalInt++;
+            } else {
+               printf("Huh?\n");
+            }
+         }
+      } else if (XorY == "Y") { //Parallel to the Y-axis
+         for (int p = 1; p < pointDist; p++) {
+            if (aX < bX) {
+               // cout << "aX+p: " << aX+p << ", aY: " << aY << "\n";
+               totalVTXArr[totalInt] = Vertex(aX+p, aY);
+               totalInt++;
+            } else if (aX > bX) {
+               // cout << "bX+p: " << bX+p << ", aY: " << aY << "\n";
+               totalVTXArr[totalInt] = Vertex(bX+p, aY);
+               totalInt++;
+            } else {
+               printf("Huh?\n");
+            }
+         }
+      }
+      //Next edge
+   }
+
+   string result = "";
+   for (int i = 0; i < edgePts; i++) {
+      result = result + "[" + to_string(totalVTXArr[i].getVertexX()) + "," + to_string(totalVTXArr[i].getVertexY()) + "]\n";
+   }
+   return result;
 }
 
 //virtual functions
@@ -63,14 +237,13 @@ float Cross::computeArea(){
    }
 
    crsArea = abs(crsArea/2.0);
-   //TODO
    return crsArea;
 }
 
 string Cross::toString(){
    string allString = "";
    allString = basicToString();
-   allString = allString + "\nVertices:\n" + getVertices() + "\n\n";
+   allString = allString + "\nVertices:\n" + getVertices() + "\nPoints on shape:\n" + getPtOn() + "\nPoints in shape:\n" + getPtIn();
    return allString;
 }
 
@@ -132,7 +305,6 @@ bool Cross::isPointInShape(Vertex vtx){
       cout << "Point exceeds shape " << name << endl;
       return false;
    }
-   cout << "C2" << endl;
    for (int i = 0; i < noOfVertices; i++) {
       int j = i + 1;
       if (i == noOfVertices-1) {
@@ -187,7 +359,6 @@ bool Cross::isPointInShape(Vertex vtx){
          }
       }
    }
-   cout << "C3" << endl;
    if (noOfIntersections < 1 || noOfIntersections == 2) {
       cout << "Point is not in shape" << endl;
       return false;
@@ -201,7 +372,6 @@ bool Cross::isPointInShape(Vertex vtx){
 }
 
 bool Cross::isPointOnShape(Vertex vtx){
-   //TODO
    int pointX = vtx.getVertexX();
    int pointY = vtx.getVertexY();
 
--- Rectangle.cpp
+++ Rectangle.cpp
@@ -48,7 +48,85 @@ string Rectangle::getVertices(){
 }
 
 string Rectangle::getPtIn(){
-   string result;
+   string result = "";
+   int xMin, yMin, xMax, yMax;
+   int xRange, yRange;
+
+   //Get min/max range of shape
+   for (int i = 0; i < noOfVertices; i++) {
+      int j = i + 1;
+      if (i == noOfVertices-1) {
+         j = 0;
+      }
+      int aX = vtxArray[i]->getVertexX();
+      int bX = vtxArray[j]->getVertexX();
+      int aY = vtxArray[i]->getVertexY();
+      int bY = vtxArray[j]->getVertexY();
+
+      if (i == 0) {
+         xMin = aX;
+         xMax = aX;
+         yMin = aY;
+         yMax = aY;
+      }
+
+      //Gets the max and min points for the shape
+      if (aX < xMin) {
+         xMin = aX;
+      } else if (bX < xMin) {
+         xMin = bX;
+      }
+
+      if (aY < yMin) {
+         yMin = aY;
+      } else if (bY < yMin) {
+         yMin = bY;
+      }
+
+      if (aX > xMax) {
+         xMax = aX;
+      } else if (bX > xMax) {
+         xMax = bX;
+      }
+
+      if (aY > yMax) {
+         yMax = aY;
+      } else if (bY > yMax) {
+         yMax = bY;
+      }
+   } // END of for loop
+
+   //Get number of points in Rectangle
+   xRange = xMax-xMin-1;
+   yRange = yMax-yMin-1;
+   if (!xRange > 0) {
+      return "1";
+   }
+
+   if (!yRange > 0) {
+      return "1";
+   }
+   int inShapeSize = xRange * yRange;
+   cout << "inShapeSize: " << inShapeSize << endl;
+
+   Vertex inShapeVtxArr[inShapeSize];
+   int inShapeItr = 0;
+
+   //Obtains points in shape from top-down, left-right
+   for (int y = 1; y < yRange+1; y++) {
+      for (int x = 1; x < xRange+1; x++) {
+         inShapeVtxArr[inShapeItr] = Vertex(xMin+x, yMin+y);
+         inShapeItr++;
+         if (inShapeItr > inShapeSize) {
+            cout << "For loop should be ending now." << endl;
+         }
+      }
+   }
+
+   for (int i = 0; i < inShapeSize; i++) {
+      result = result + "[" + to_string(inShapeVtxArr[i].getVertexX()) + "," + to_string(inShapeVtxArr[i].getVertexY()) + "]\n";
+   }
+
    return result;
 }
 
@@ -73,17 +151,17 @@ string Rectangle::getPtOn(){
          edgePts = edgePts + abs(aX - bX) - 1;
       } else {
          cout << "Error: shape cannot be computed!" << endl;
-         return "0";
+         return "1";
       }
    } // end for loop
 
    if (!edgePts > 0) {
-      cout << "Error: shape cannot be computed!" << endl;
-      return "0";
+      cout << "Error: No points on shape!" << endl;
+      return "1";
    }
 
    cout << "edgePts: " << edgePts << endl;
-   Vertex *totalVTXArr[edgePts];
+   Vertex totalVTXArr[edgePts];
    int totalInt = 0;
    for (int i = 0; i < noOfVertices; i++) {
       int j = i + 1;
@@ -103,27 +181,27 @@ string Rectangle::getPtOn(){
          pointDist = abs(aY - bY);
          XorY = "X";
       } else if (aY == bY) {
-         pointDist = abs(aX - bY);
+         pointDist = abs(aX - bX);
          XorY = "Y";
       } else {
-         cout << "Error: shape cannot be computed!" << endl;
-         return "0";
+         cout << "Error: Side is slanted!" << endl;
+         return "1";
       }
 
       if (pointDist == 0) {
-         cout << "Error: shape cannot be computed!" << endl;
-         return "0";
+         cout << "Error: Side has no points!" << endl;
+         return "1";
       }
       // cout << "pointDist: " << pointDist << endl;
       if (XorY == "X") { //Parallel to the X-axis
          for (int p = 1; p < pointDist; p++) {
             if (aY < bY) {
                // cout << "aX: " << aX << ", aY+p: " << aY+p << "\n";
-               totalVTXArr[totalInt] = new Vertex(aX, aY+p);
+               totalVTXArr[totalInt] = Vertex(aX, aY+p);
                totalInt++;
             } else if (aY > bY) {
                // cout << "aX: " << aX << ", bY+p: " << bY+p << "\n";
-               totalVTXArr[totalInt] = new Vertex(aX, bY+p);
+               totalVTXArr[totalInt] = Vertex(aX, bY+p);
                totalInt++;
             } else {
                printf("Huh?\n");
@@ -133,11 +211,11 @@ string Rectangle::getPtOn(){
          for (int p = 1; p < pointDist; p++) {
             if (aX < bX) {
                // cout << "aX+p: " << aX+p << ", aY: " << aY << "\n";
-               totalVTXArr[totalInt] = new Vertex(aX+p, aY);
+               totalVTXArr[totalInt] = Vertex(aX+p, aY);
                totalInt++;
             } else if (aX > bX) {
                // cout << "bX+p: " << bX+p << ", aY: " << aY << "\n";
-               totalVTXArr[totalInt] = new Vertex(bX+p, aY);
+               totalVTXArr[totalInt] = Vertex(bX+p, aY);
                totalInt++;
             } else {
                printf("Huh?\n");
@@ -149,7 +227,7 @@ string Rectangle::getPtOn(){
 
    string result = "";
    for (int i = 0; i < edgePts; i++) {
-      result = result + "[" + to_string(totalVTXArr[i]->getVertexX()) + "," + to_string(totalVTXArr[i]->getVertexY()) + "]\n";
+      result = result + "[" + to_string(totalVTXArr[i].getVertexX()) + "," + to_string(totalVTXArr[i].getVertexY()) + "]\n";
    }
    return result;
 }
@@ -170,12 +248,11 @@ float Rectangle::computeArea(){
 string Rectangle::toString(){
    string allString = "";
    allString = basicToString();
-   allString = allString + "\nVertices:\n" + getVertices() + "\nPoints on shape:\n" + getPtOn();
+   allString = allString + "\nVertices:\n" + getVertices() + "\nPoints on shape:\n" + getPtOn() + "\nPoints in shape:\n" + getPtIn();
    return allString;
 }
 
 bool Rectangle::isPointInShape(Vertex vtx){
-   //TODO
    int pointX = vtx.getVertexX();
    int pointY = vtx.getVertexY();
    int xMin, yMin, xMax, yMax;
@@ -238,7 +315,6 @@ bool Rectangle::isPointOnShape(Vertex vtx){
    //TODO
    int pointX = vtx.getVertexX();
    int pointY = vtx.getVertexY();
-   cout << "C1" << endl;
    for (int i = 0; i < noOfVertices; i++) {
       int j = i + 1;
       if (i == noOfVertices-1) {
@@ -254,10 +330,10 @@ bool Rectangle::isPointOnShape(Vertex vtx){
 
       //If point is on vertices
       if (pointX == aX && pointY == aY) {
-         cout << "ON VERTIX: Entry [" << pointX << ":" << pointY << "]: " << aX << ":" << aY << endl;
+         cout << "ON VERTEX: Entry [" << pointX << ":" << pointY << "]: " << aX << ":" << aY << endl;
          return false;
       } else if (pointX == bX && pointY == bY) {
-         cout << "ON VERTIX: Entry [" << pointX << ":" << pointY << "]: " << bX << ":" << bY << endl;
+         cout << "ON VERTEX: Entry [" << pointX << ":" << pointY << "]: " << bX << ":" << bY << endl;
          return false;
       }
       //Checks for number of points which describe the edges of the Rectangle
@@ -265,7 +341,7 @@ bool Rectangle::isPointOnShape(Vertex vtx){
          pointDist = abs(aY - bY);
          XorY = "X";
       } else if (aY == bY) {
-         pointDist = abs(aX - bY);
+         pointDist = abs(aX - bX);
          XorY = "Y";
       } else {
          cout << "Error: shape cannot be computed!" << endl;
@@ -276,7 +352,6 @@ bool Rectangle::isPointOnShape(Vertex vtx){
          cout << "Error: shape cannot be computed!" << endl;
          return false;
       }
-      cout << "C2" << endl;
 
       //Vertex tmpVTXArr[pointDist];
       if (XorY == "X" && pointX == aX) {
@@ -304,7 +379,6 @@ bool Rectangle::isPointOnShape(Vertex vtx){
             }
          }
       }
-      cout << "C3" << endl;
 
       //Next edge
    } //end for loop
